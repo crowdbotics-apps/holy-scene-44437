1e420b4094f225c52502118e094bc5c6ef5dd4e1
Crowdbotics <team@crowdbotics.com>
2023-11-09 11:48:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-scene-44437",
+  url: "https://holy-scene-44437.botics.co",
+  api: "https://holy-scene-44437.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
